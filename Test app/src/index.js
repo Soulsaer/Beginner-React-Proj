f0af4e504fd9faae25a1fbbfba55006be7513e4b
Soulsaer <bhuvneshg785@gmail.com>
2024-05-17 23:48:20 +0530

    empty the project

--- Test app/src/index.js
+++
@@ -1,4 +0,0 @@
-const div  = "<h1>this is text</h1>"
-div 
-
-return div;
--- page/index.js
+++
@@ -1,5 +0,0 @@
-const div = React.createElement('div' , {} , "hello world");
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(div);
-
